ccb3d848484e9bab1fba3445b409749f1b2f3859
Iryna Yehorova <yehorova.dev@gmail.com>
2024-07-21 10:35:33 +0300

    make build

---
+++ index.ebbba975.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),l=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){e.preventDefault();var t,r=null===(t=e.target.closest(".list-item__link"))||void 0===t?void 0:t.href;l.src=r});
+//# sourceMappingURL=index.ebbba975.js.map
